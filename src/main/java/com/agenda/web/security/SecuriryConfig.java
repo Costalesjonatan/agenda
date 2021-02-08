5319a90b243f4b9246484913ce15a9ba4e1b40fc
Costalesjonatan <42694800+Costalesjonatan@users.noreply.github.com>
2021-02-07 21:51:45 -0300

    modificación de la clase SecurityConfig

--- src/main/java/com/agenda/web/security/SecuriryConfig.java
+++ src/main/java/com/agenda/web/security/SecuriryConfig.java
@@ -1,16 +1,20 @@
 package com.agenda.web.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecuriryConfig extends WebSecurityConfigurerAdapter
 {
-
+	@Autowired
+	private UserDetailsService userDetailService;
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		// TODO Auto-generated method stub
