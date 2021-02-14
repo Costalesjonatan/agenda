5a8988119af46d4c22f7227280acc0a92a2f0551
Costalesjonatan <42694800+Costalesjonatan@users.noreply.github.com>
2021-02-14 15:58:24 -0300

    se agrega test del ContactService que falla

---
+++ src/test/java/com/agenda/web/ContactServiceTest.java
@@ -0,0 +1,45 @@
+package com.agenda.web;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import com.agenda.web.implementation.ContactServiceImplementation;
+import com.agenda.web.model.Contact;
+import com.agenda.web.repository.ContactDAOInterface;
+
+class ContactServiceTest {
+	
+	private ContactServiceImplementation contactService;
+	private ContactDAOInterface contactDAO;
+
+	@Test
+	public void shouldCreateContact() {
+		giverContactService();
+		givenContactDAO();
+		
+		contactService.createContact(Contact.builder()
+				.id(1)
+				.name("jonatan")
+				.number("1139586203")
+				.idUser(1)
+				.build());
+		
+		Optional<Contact> contactCreated = contactDAO.findById((long) 1);
+		Contact contact = contactCreated.get();
+		
+		assertTrue(contact.getName().equals("jonatan") && contact.getNumber().equals("1139586203") && contact.getIdUser() == 1);
+	}
+	
+	private void giverContactService() {
+		contactService = new ContactServiceImplementation();
+	}
+	
+	private void givenContactDAO() {
+		contactDAO = new MockContactDAO();
+	}
+	
+}
---
+++ src/test/java/com/agenda/web/MockContactDAO.java
@@ -0,0 +1,170 @@
+package com.agenda.web;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.agenda.web.model.Contact;
+import com.agenda.web.repository.ContactDAOInterface;
+import com.demo.proyectoBackend.model.User;
+
+public class MockContactDAO implements ContactDAOInterface{
+	
+	private HashMap<Long, Contact> contacts = new HashMap<Long, User>();
+
+	@Override
+	public List<Contact> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Contact> findAll(Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Contact> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Contact> List<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends Contact> S saveAndFlush(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteInBatch(Iterable<Contact> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Contact getOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Contact> List<S> findAll(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Contact> List<S> findAll(Example<S> example, Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Contact> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Contact> S save(S entity) {
+		contacts.put(entity.getId(), entity)
+		return null;
+	}
+
+	@Override
+	public Optional<Contact> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Contact entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Contact> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends Contact> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Contact> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Contact> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends Contact> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Contact> getAllContactsOfAuserForId(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
